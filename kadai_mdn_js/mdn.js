a4eda725af2206406b662128887f8f939b361436
nakano302 <yukko_o302@yahoo.co.jp>
2025-08-15 01:42:13 +0900

    kadai_mdn reupload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,2 +1,7 @@
-let now = new Date();
-console.log(now);
+var today = new Date();
+
+var year = today.getFullYear();
+var month = today.getMonth() + 1;
+var day = today.getDate();
+
+console.log(year + "年" + month + "月" + day + "日");
