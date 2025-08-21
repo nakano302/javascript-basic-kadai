24828bec128568bb583bc72bf22c6ef439b6367c
nakano302 <yukko_o302@yahoo.co.jp>
2025-08-22 01:54:53 +0900

    kadai_020upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,10 +1,7 @@
 // btnというidを持つHTML要素を取得し、定数に代入する
 const btn = document.getElementById("btn");
+const text = document.getElementById("text");
 
-// HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener("click", () => {
-  console.log("ボタンをクリックしました");
-
-  // 作成したli要素に「ボタンをクリックしました」というテキストを追加する
-  textElement.textContent = "ボタンをクリックしました";
+  text.textContent = "ボタンをクリックしました";
 });
